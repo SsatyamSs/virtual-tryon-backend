7275416244ce1b65467471f7c7a20585707130e4
SsatyamSs <160154956+SsatyamSs@users.noreply.github.com>
2026-04-27 13:09:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.get("/result", async (req, res) => {
 // ✅ PORT FIX
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0" , () => {
     console.log(`🔥 Server running on port ${PORT}`);
 }); 
